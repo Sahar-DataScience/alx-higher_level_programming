ca0a57c4692a1855339571e3c9c193801d72aa83
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2023-12-08 02:57:16 -0800

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+import numpy
+
+def square_matrix_simple(matrix=[]):
+
+    m = len(matrix)
+    n = len(matrix[0])
+    for i in range(m):
+        for j in range(n):
+            matrix[i][j] = matrix[i][j]**2
+    return matrix
