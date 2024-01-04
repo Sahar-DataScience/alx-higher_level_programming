52b26ec1525cbcce570d1e63d2424fdb060a2e27
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-04 05:17:59 -0800

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try :
-        line =''
+    try:
+        line = ''
         for i in range(x):
             line += str(my_list[i])
-        print(line,'\n')
+        print(line, '\n')
 
     except IndexError:
         print("x should be integer and less or equal to list length")
