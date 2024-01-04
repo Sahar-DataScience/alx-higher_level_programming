10b7fdd744c7918b6b8fdeb746ba63f18edd1234
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-04 06:31:59 -0800

    hi

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         line = ''
         for i in range(x):
-            print(my_list[i], end='')
+            line += str(my_list[i])
             nb_digit += 1
 
     except IndexError:
         pass
+    print(line)
     return nb_digit
