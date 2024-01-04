02c0c47898dd88f94f9e6e87a688fcc95842be27
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-04 13:23:13 -0800

    hello

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,11 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     nb_digit = 0
-    line = ''
     for i in range(x):
         try:
-            line += str(my_list[i])
+            print("{:d}".format(my_list[i]), end='')
             nb_digit += 1
         except (TypeError, ValueError):
             pass
-    print(line)
+    print()
     return nb_digit
