cbd1d08842796f3952498fc17118e9b8b01e9a02
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-04 05:38:07 -0800

    error fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             nb_digit += 1
         print(line)
     except IndexError:
-        continue
+        print(" x out of range of list")
     return nb_digit
